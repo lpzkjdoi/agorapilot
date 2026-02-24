4aa126cf8d86266f913f464fea3bd754b69cb353
lpzkjdoi <lpzkjdoi@icloud.com>
2026-02-24 19:23:24 +0100

    chore(config): config added for JPA

---
+++ apps/back/src/main/java/fr/maximechazard/agorapilot/back/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package fr.maximechazard.agorapilot.back.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+}
