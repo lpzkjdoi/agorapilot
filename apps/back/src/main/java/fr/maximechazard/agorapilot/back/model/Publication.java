082e20cfc7f71c26986d6648fe87ec33fa99faf0
lpzkjdoi <lpzkjdoi@icloud.com>
2026-02-24 19:41:50 +0100

    feat: Publication entity added

--- apps/back/src/main/java/fr/maximechazard/agorapilot/back/model/Publication.java
+++ apps/back/src/main/java/fr/maximechazard/agorapilot/back/model/Publication.java
@@ -6,7 +6,6 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
