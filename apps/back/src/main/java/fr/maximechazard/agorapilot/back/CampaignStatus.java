17e3fd6e80c35c3aa36c15d9e2c5a850a570a0a0
lpzkjdoi <lpzkjdoi@icloud.com>
2026-02-27 18:43:50 +0100

    feat: Campaign entity added

---
+++ apps/back/src/main/java/fr/maximechazard/agorapilot/back/CampaignStatus.java
@@ -0,0 +1,8 @@
+package fr.maximechazard.agorapilot.back;
+
+public enum CampaignStatus {
+    DRAFT,
+    ACTIVE,
+    COMPLETED,
+    CANCELED,
+}
--- apps/back/src/main/java/fr/maximechazard/agorapilot/back/model/Campaign.java
+++ apps/back/src/main/java/fr/maximechazard/agorapilot/back/model/Campaign.java
@@ -1,7 +1,9 @@
 package fr.maximechazard.agorapilot.back.model;
 
-import fr.maximechazard.agorapilot.back.PublicationStatus;
+import fr.maximechazard.agorapilot.back.CampaignStatus;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.FutureOrPresent;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
@@ -11,36 +13,39 @@ import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
-@Table(name = "publications")
+@Table(name = "campaigns")
 @NoArgsConstructor
 @Data
 @EntityListeners(AuditingEntityListener.class)
-public class Publication {
+public class Campaign {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @NotBlank
+    @Column(nullable = false)
+    private String name;
+
     @Column(columnDefinition = "TEXT", nullable = false)
-    private String content;
+    private String description;
+
+    @FutureOrPresent
+    private LocalDateTime startDate;
+
+    @Future
+    private LocalDateTime endDate;
 
     @NotNull
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
-    private PublicationStatus status;
+    private CampaignStatus status;
 
-    private LocalDateTime scheduledAt;
-
-    private LocalDateTime publishedAt;
-
-    private LocalDateTime endAt;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "campaign_id")
-    private Campaign campaign;
-
+    @OneToMany(mappedBy = "campaign", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Publication> publications = new ArrayList<>();
 
     @Column(nullable = false)
     private Boolean archived = false;
@@ -51,12 +56,4 @@ public class Publication {
 
     @LastModifiedDate
     private LocalDateTime updatedAt;
-
-    public Publication(String content, LocalDateTime scheduledAt, LocalDateTime publishedAt, LocalDateTime endAt) {
-        this.content = content;
-        this.scheduledAt = scheduledAt;
-        this.publishedAt = publishedAt;
-        this.endAt = endAt;
-        this.status = PublicationStatus.DRAFT;
-    }
 }
