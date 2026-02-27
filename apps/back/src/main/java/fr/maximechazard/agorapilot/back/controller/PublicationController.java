8c3e77142bded55475e298fa2395c2b6baec7227
lpzkjdoi <lpzkjdoi@icloud.com>
2026-02-27 17:45:18 +0100

    feat: POST route for Publication creation added

--- apps/back/src/main/java/fr/maximechazard/agorapilot/back/controller/PublicationController.java
+++ apps/back/src/main/java/fr/maximechazard/agorapilot/back/controller/PublicationController.java
@@ -1,13 +1,13 @@
 package fr.maximechazard.agorapilot.back.controller;
 
 import fr.maximechazard.agorapilot.back.model.Publication;
+import fr.maximechazard.agorapilot.back.request.CreatePublicationRequest;
 import fr.maximechazard.agorapilot.back.service.PublicationService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/api/publications")
 @RequiredArgsConstructor
@@ -15,8 +15,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class PublicationController {
     private final PublicationService publicationService;
 
+    // -------------------------------- GET --------------------------------
     @GetMapping
     public ResponseEntity<Iterable<Publication>> getAll() {
         return new ResponseEntity<>(publicationService.getAll(), HttpStatus.OK);
+    }
+
+    // -------------------------------- POST --------------------------------
+    @PostMapping
+    public ResponseEntity<Publication> create(@RequestBody @Valid CreatePublicationRequest request) {
+        return new ResponseEntity<>(publicationService.create(request), HttpStatus.CREATED);
     }
 }
