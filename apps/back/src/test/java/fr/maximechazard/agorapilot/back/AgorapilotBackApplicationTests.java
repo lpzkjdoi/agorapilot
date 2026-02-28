351bfe9aa288d60437938d5e74970ace437079fc
lpzkjdoi <lpzkjdoi@icloud.com>
2026-02-28 12:10:17 +0100

    fix: configuration fixed for tests

--- apps/back/src/test/java/fr/maximechazard/agorapilot/back/AgorapilotBackApplicationTests.java
+++ apps/back/src/test/java/fr/maximechazard/agorapilot/back/AgorapilotBackApplicationTests.java
@@ -3,11 +3,18 @@ package fr.maximechazard.agorapilot.back;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+        "spring.datasource.url=jdbc:h2:mem:agorapilot_test;DB_CLOSE_DELAY=-1;MODE=PostgreSQL",
+        "spring.datasource.driverClassName=org.h2.Driver",
+        "spring.datasource.username=sa",
+        "spring.datasource.password=",
+        "spring.jpa.hibernate.ddl-auto=create-drop",
+        "spring.jpa.show-sql=false"
+})
 class AgorapilotBackApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
