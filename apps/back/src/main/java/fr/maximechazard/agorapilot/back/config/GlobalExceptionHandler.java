0b92b108bfe7ca192ef6cb7935a2b90dbf61153e
lpzkjdoi <lpzkjdoi@icloud.com>
2026-03-13 19:46:37 +0100

    feat(exceptions): add exceptions for publication and campaign not found

--- apps/back/src/main/java/fr/maximechazard/agorapilot/back/config/GlobalExceptionHandler.java
+++ apps/back/src/main/java/fr/maximechazard/agorapilot/back/config/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package fr.maximechazard.agorapilot.back.config;
 
 import fr.maximechazard.agorapilot.back.campaign.exceptions.CampaignNotFoundException;
+import fr.maximechazard.agorapilot.back.publication.exceptions.PublicationNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -8,9 +9,17 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-    
+
     @ExceptionHandler(CampaignNotFoundException.class)
     public ResponseEntity<ApiError> handleCampaignNotFoundException(CampaignNotFoundException exception) {
+        ApiError apiError = new ApiError();
+        apiError.setMessage(exception.getMessage());
+        apiError.setCode(HttpStatus.NOT_FOUND.value());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(apiError);
+    }
+
+    @ExceptionHandler(PublicationNotFoundException.class)
+    public ResponseEntity<ApiError> handlePublicationNotFoundException(PublicationNotFoundException exception) {
         ApiError apiError = new ApiError();
         apiError.setMessage(exception.getMessage());
         apiError.setCode(HttpStatus.NOT_FOUND.value());
