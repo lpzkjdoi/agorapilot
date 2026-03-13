55cb346dedd2fbf4388061a51f7b59d6033bc3c7
lpzkjdoi <lpzkjdoi@icloud.com>
2026-03-13 18:38:56 +0100

    feat(exceptions): add exceptions handling

---
+++ apps/back/src/main/java/fr/maximechazard/agorapilot/back/config/GlobalExceptionHandler.java
@@ -0,0 +1,19 @@
+package fr.maximechazard.agorapilot.back.config;
+
+import fr.maximechazard.agorapilot.back.campaign.exceptions.CampaignNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    
+    @ExceptionHandler(CampaignNotFoundException.class)
+    public ResponseEntity<ApiError> handleCampaignNotFoundException(CampaignNotFoundException exception) {
+        ApiError apiError = new ApiError();
+        apiError.setMessage(exception.getMessage());
+        apiError.setCode(HttpStatus.NOT_FOUND.value());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(apiError);
+    }
+}
