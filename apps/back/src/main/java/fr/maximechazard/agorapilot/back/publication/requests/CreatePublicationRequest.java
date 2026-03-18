b66c29cef4883b8dd5746d138f14624207cf09a1
lpzkjdoi <lpzkjdoi@icloud.com>
2026-03-18 15:47:42 +0100

    fix(back): CreatePublicationRequest updated to fit new pattern

--- apps/back/src/main/java/fr/maximechazard/agorapilot/back/publication/requests/CreatePublicationRequest.java
+++ apps/back/src/main/java/fr/maximechazard/agorapilot/back/publication/requests/CreatePublicationRequest.java
@@ -1,12 +1,8 @@
 package fr.maximechazard.agorapilot.back.publication.requests;
 
-import jakarta.validation.constraints.Future;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.PastOrPresent;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
-
-import java.time.LocalDateTime;
 
 @Getter
 @SuppressWarnings("unused")
@@ -15,13 +11,4 @@ public class CreatePublicationRequest {
     @NotBlank
     @Size(max = 10000)
     private String content;
-
-    @Future
-    private LocalDateTime scheduledAt;
-
-    @PastOrPresent
-    private LocalDateTime publishedAt;
-
-    @Future
-    private LocalDateTime endAt;
 }
