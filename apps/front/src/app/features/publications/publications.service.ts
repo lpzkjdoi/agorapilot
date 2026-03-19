15db6496b27a9b36a2665b9e57334d1584d8eb0c
lpzkjdoi <lpzkjdoi@icloud.com>
2026-03-19 18:34:42 +0100

    fix(front): fix api url handling

--- apps/front/src/app/features/publications/publications.service.ts
+++ apps/front/src/app/features/publications/publications.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from "../../../environments/environment";
 import { CreatePublicationRequest, Publication } from "./publication.model";
 
 @Injectable({
@@ -8,9 +9,7 @@ import { CreatePublicationRequest, Publication } from "./publication.model";
 })
 export class PublicationsService {
   private readonly http = inject(HttpClient);
-  private readonly apiUrl = 'http://localhost:8080/api/publications';
-
-  private readonly url = this.apiUrl + "/publications"
+  private readonly url = environment.apiUrl + "/publications"
 
   getPublications(): Observable<Publication[]> {
     return this.http.get<Publication[]>(this.url);
