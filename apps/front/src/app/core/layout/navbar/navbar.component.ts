87386b19409836f8db5d055d1d20f21f8440f7a4
lpzkjdoi <lpzkjdoi@icloud.com>
2026-07-26 18:22:42 +0200

    changements de logo et fix de du sticky dans la navbar

--- apps/front/src/app/core/layout/navbar/navbar.component.ts
+++ apps/front/src/app/core/layout/navbar/navbar.component.ts
@@ -1,20 +1,14 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { RouterLink, RouterLinkActive } from "@angular/router";
-import {
-  NavbarButtonComponent,
-} from "./components/navbar-button/navbar-button.component";
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { NavbarButtonComponent } from './components/navbar-button/navbar-button.component';
+import { LogoComponent } from '../../logo/logo.component';
 
 @Component({
   selector: 'app-navbar',
-  imports: [
-    NavbarButtonComponent,
-    RouterLink,
-    RouterLinkActive,
-  ],
+  imports: [NavbarButtonComponent, RouterLink, RouterLinkActive, LogoComponent],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css',
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class NavbarComponent {
-}
+export class NavbarComponent {}
